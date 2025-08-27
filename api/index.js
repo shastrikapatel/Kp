a611bb2e4ae498615a7723bef5221d6f2204b776
shastrikapatel <shastrikapatel24@gmail.com>
2025-08-27 16:47:06 +0530

    new

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const bodyParser = require("body-parser");
-const session = require("express-session");
+const path = require("path");
 
 const app = express();
 
+// MongoDB connection
+const MONGO_URL = "mongodb+srv://kp:kp%4012@cluster0.3fnyzv1.mongodb.net/priceListDB?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
-
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "..", "views"));
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.set("views", path.join(__dirname, "../views"));
 
 // Routes
 const adminRoutes = require("../routes/admin");
@@ -27,5 +26,5 @@ const customerRoutes = require("../routes/customer");
 app.use("/admin", adminRoutes);
 app.use("/", customerRoutes);
 
-// Export as Vercel serverless function
+// Export for Vercel
 module.exports = app;
