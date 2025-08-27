b0f089c0310a06dc03416d1c4da46f89d8fa9ab2
shastrikapatel <shastrikapatel24@gmail.com>
2025-08-28 00:20:23 +0530

    new

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const session = require("express-session");
+
+const app = express();
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Session setup
+app.use(session({
+    secret: "secretkey123",
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Routes
+const adminRoutes = require("../routes/admin");
+const customerRoutes = require("../routes/customer");
+
+app.use("/admin", adminRoutes);
+app.use("/customer", customerRoutes);
+
+// Redirect root to login
+app.get("/", (req, res) => res.redirect("/admin/login"));
+
+// Export the app for Vercel
+module.exports = app;
