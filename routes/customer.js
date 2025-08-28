256ef275420c6a02b74a75a81792d3b7c9217958
shastrikapatel <shastrikapatel24@gmail.com>
2025-08-28 14:25:32 +0530

    new

--- routes/customer.js
+++ routes/customer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../models/Item");
 
-// Customer Panel
+// Customer view
 router.get("/", async (req, res) => {
     const items = await Item.find();
     res.render("customer", { items });
